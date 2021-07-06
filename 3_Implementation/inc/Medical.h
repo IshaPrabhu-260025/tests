aff78c548f087956ec55051494ed54fc8b6a1bd5
Isha Prabhu <67497698+IshaPrabhu-260025@users.noreply.github.com>
2021-07-06 16:22:39 +0530

    Create Medical.h

---
+++ 3_Implementation/inc/Medical.h
@@ -0,0 +1,108 @@
+/**
+ * @file Medical.h
+ * @author Team 6 (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-07-03
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef MEDICAL_H_INCLUDED
+#define MEDICAL_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void delay(int number_of_seconds);
+
+/**
+ * Welcome screen
+ * @param[in] x and y is argument
+ * @return void
+ */
+
+
+/**
+ * Welcome screen
+ *
+ * @return void
+ */
+
+void WelcomeScreen(void);
+
+/**
+ * For Title
+ * @return void
+ */
+
+void Title(void);
+
+/**
+ * To display Main Menu
+ *
+ * @return void
+ */
+void MainMenu(void);
+
+/**
+ * To display Main Menu
+ *
+ * @return void
+ */
+
+void LoginScreen(void);
+
+/**
+ * To Add records
+ *
+ * @return void
+ */
+
+void Add_rec(void);
+/**
+ * To display list
+ *
+ * @return void
+ */
+
+void func_list();
+
+/**
+ * To Search
+ *
+ * @return void
+ */
+
+void Search_rec(void);
+
+/**
+ * To display Edit
+ *
+ * @return void
+ */
+
+void Edit_rec(void);
+
+/**
+ * To Delete
+ *
+ * @return void
+ */
+
+
+void Dlt_rec(void);
+
+
+/**
+ * To Exit
+ *
+ * @return void
+ */
+
+void ex_it(void);
+
+
+
+#endif // MEDICAL_H_INCLUDED
